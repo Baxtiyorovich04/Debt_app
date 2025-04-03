26db84a51bdbef3d99cbcfee8418c1fbee456d46
Javohir <markpomidorchik@gmail.com>
2025-04-03 20:19:49 +0500

    check:

--- src/utils/API.tsx
+++ src/utils/API.tsx
@@ -13,6 +13,7 @@ API.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
     if (token) {
         config.headers.Authorization = `Bearer ${token}`;
+        console.log(config.headers.Authorization);
     }
     return config;
 });
